95cb07ed8094a83c13cfb49ca1be2d091e8034e8
OscarRobertLai <140251339+OscarRobertLai@users.noreply.github.com>
2023-11-25 17:37:01 +1100

    Simple drag and drop functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  // Handler for dropping files
+  const dropHandler = (event) => {
+    event.preventDefault();
+  
+    if (event.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      for (let i = 0; i < event.dataTransfer.items.length; i++) {
+        if (event.dataTransfer.items[i].kind === 'file') {
+          const file = event.dataTransfer.items[i].getAsFile();
+          console.log('File name: ', file.name);
+        }
+      }
+    } else {
+      // Use DataTransfer interface to access the file(s)
+      for (let i = 0; i < event.dataTransfer.files.length; i++) {
+        console.log('File name: ', event.dataTransfer.files[i].name);
+      }
+    }
+  };
+
+  // Prevent default behavior (Prevent file from being opened)
+  const dragOverHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      id="drop_zone"
+      onDrop={dropHandler}
+      onDragOver={dragOverHandler}
+      style={{ border: '2px dashed #ccc', padding: '20px', textAlign: 'center' }}>
+      <p>Drag one or more files to this <i>drop zone</i>.</p>
     </div>
   );
 }
-
-export default App;
