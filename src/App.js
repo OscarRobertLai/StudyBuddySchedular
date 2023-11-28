9b0deac2594085b583a4b36f8814d37cfc65f142
Oscar Lai <oscarlai2@gmail.com>
2023-11-28 16:07:25 +1100

    Oscar brnach test

--- src/App.js
+++ src/App.js
@@ -6,45 +6,6 @@ import DropBox from "./components/DropBox"
 import TestButton from './components/TestButton';
 
 
-// const DetermineTime = ({ events }) => {
-//   const [time1, setTime1] = useState('09:00');
-//   const [time2, setTime2] = useState('17:00');
-
-//   const handleCalculation = (e) => {
-//     e.preventDefault(); // Prevent the default form submit action
-//     console.log( time1, time2 )
-//   }
-  
-//   const handleTime1Change = (e) => {
-//     setTime1(e.target.value);
-//   };
-
-//   const handleTime2Change = (e) => {
-//     setTime2(e.target.value);
-//   };
-
-//   return (
-//     <form onSubmit={handleCalculation}>
-//       {/* First Hour Picker */}
-//       <div>
-//         <label htmlFor="timePicker1">Select Hour 1:</label>
-//         <input type="time" id="timePicker1" name="timePicker1" value={time1} onChange={handleTime1Change} />
-//       </div>
-
-//       {/* Second Hour Picker */}
-//       <div>
-//         <label htmlFor="timePicker2">Select Hour 2:</label>
-//         <input type="time" id="timePicker2" name="timePicker2" value={time2} onChange={handleTime2Change} />
-//       </div>
-
-//       {/* Submit Button */}
-//       <div>
-//         <button type="submit">Submit</button>
-//       </div>
-//     </form>
-//   );
-// }
-
 export default function App (){ 
   const [selected, setSelected] = useState();
   const [isDragOver, setIsDragOver] = useState(false); // State to track drag over status
@@ -79,3 +40,5 @@ export default function App (){
     );
 
   }
+
+  
